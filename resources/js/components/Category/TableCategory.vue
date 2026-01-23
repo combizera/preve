aa4531a775439a2bb74a8ceefddcdf7bc82161fc
combizera <ycombi349@gmail.com>
2026-01-23 11:32:41 -0300

    feat: enhance TableCategory component with action buttons and improved layout

--- resources/js/components/Category/TableCategory.vue
+++ resources/js/components/Category/TableCategory.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { Trash, Edit } from 'lucide-vue-next';
+
+import { Button } from '@/components/ui/button';
 import {
     Table,
     TableBody,
@@ -8,6 +11,8 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
+import { getColorClass } from '@/lib/category-colors';
+import { getIconComponent } from '@/lib/category-icons';
 import { Category } from '@/types/models/category';
 
 defineProps<{
@@ -20,20 +25,49 @@ defineProps<{
         <TableCaption>A list yours categories</TableCaption>
         <TableHeader>
             <TableRow>
-                <TableHead class="w-[100px]"> Name </TableHead>
-                <TableHead>Slug</TableHead>
+                <TableHead> Name </TableHead>
                 <TableHead>Description</TableHead>
-                <TableHead>Color</TableHead>
-                <TableHead>Icon</TableHead>
+                <TableHead>Actions</TableHead>
             </TableRow>
         </TableHeader>
         <TableBody>
             <TableRow v-for="category in categories" :key="category.id">
-                <TableCell>{{ category.name }}</TableCell>
-                <TableCell>{{ category.slug }}</TableCell>
-                <TableCell>{{ category.description }}</TableCell>
-                <TableCell>{{ category.color }}</TableCell>
-                <TableCell>{{ category.icon }}</TableCell>
+                <TableCell class="flex items-center gap-3">
+                    <div
+                        :class="[
+                            getColorClass(category.color, 'bg', 950),
+                            'inline-flex items-center justify-center rounded p-1.5',
+                        ]"
+                    >
+                        <component
+                            :is="getIconComponent(category.icon)"
+                            :size="18"
+                            :class="getColorClass(category.color, 'text', 500)"
+                        />
+                    </div>
+                    <p class="font-medium">
+                        {{ category.name }}
+                    </p>
+                </TableCell>
+                <TableCell>
+                    <p class="text-sm text-muted-foreground">
+                        {{
+                            category.description && category.description.length > 25
+                                ? category.description.slice(0, 25) + '...'
+                                : category.description
+                        }}
+                    </p>
+                </TableCell>
+                <TableCell>
+                    <div class="inline-flex items-center justify-center rounded p-1.5">
+                        <Button size="sm" variant="secondary" class="mr-2">
+                            <Edit :size="18" />
+                        </Button>
+                        <Button size="sm" variant="destructive">
+                            <Trash :size="18" />
+                        </Button>
+                    </div>
+                </TableCell>
             </TableRow>
         </TableBody>
     </Table>
