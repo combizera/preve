f9fca706da433f142ed19355cae059abf30b94d4
combizera <ycombi349@gmail.com>
2026-01-23 01:20:54 -0300

    feat: implement Category management with TableCategory component

---
+++ resources/js/components/Category/TableCategory.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import { Category } from '@/types/models/category';
+
+defineProps<{
+    categories: Category[];
+}>();
+</script>
+
+<template>
+    <Table>
+        <TableCaption>A list yours categories</TableCaption>
+        <TableHeader>
+            <TableRow>
+                <TableHead class="w-[100px]"> Name </TableHead>
+                <TableHead>Slug</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead>Color</TableHead>
+                <TableHead>Icon</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow v-for="category in categories" :key="category.id">
+                <TableCell>{{ category.name }}</TableCell>
+                <TableCell>{{ category.slug }}</TableCell>
+                <TableCell>{{ category.description }}</TableCell>
+                <TableCell>{{ category.color }}</TableCell>
+                <TableCell>{{ category.icon }}</TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+</template>
--- resources/js/pages/Category.vue
+++ resources/js/pages/Category.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
 
+import TableCategory from '@/components/Category/TableCategory.vue';
+import Heading from '@/components/Heading.vue';
 import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
-import categories from '@/routes/categories';
+import { dashboard } from '@/routes';
+import categoryRoutes from '@/routes/categories';
 import { type BreadcrumbItem } from '@/types';
+
+defineProps<{
+    categories: Category[];
+}>();
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
+        title: 'Dashboard',
+        href: dashboard().url,
+    },
+    {
         title: 'Category',
-        href: categories.index().url,
+        href: categoryRoutes.index().url,
     },
 ];
 </script>
@@ -19,26 +30,18 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div
-            class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
+            class="flex h-full flex-1 flex-col gap-2 overflow-x-auto rounded-xl p-4"
         >
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    oi
-                    <PlaceholderPattern />
-                </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    <PlaceholderPattern />
-                </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    <PlaceholderPattern />
-                </div>
-            </div>
+            <!-- HEADING -->
+            <Heading
+                title="Category"
+                description="Manage your categories here."
+            />
+
+            <!-- TABLE - CRUD -->
+            <TableCategory :categories />
+
+            <!-- PLACEHOLDER -->
             <div
                 class="relative min-h-full flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border"
             >
