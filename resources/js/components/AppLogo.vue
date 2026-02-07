ae1f037ed5eddf11b5b2325b99697158cd95fef6
Victor Alves <viictor.alvess12@gmail.com>
2026-02-06 22:07:55 -0300

    fix: adjust app logo to show correctly with light theme

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -4,7 +4,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 
 <template>
   <div class="flex items-center gap-2 px-2 group-data-[collapsible=icon]:p-2!">
-    <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+    <AppLogoIcon />
     <span class="mb-0.5 font-semibold group-data-[collapsible=icon]:hidden">
       Prevê
     </span>
