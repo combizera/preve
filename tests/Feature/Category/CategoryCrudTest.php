f2234910f5a61b935f0ca44eec0b86e1ea19fc9c
combizera <ycombi349@gmail.com>
2026-02-11 19:33:30 -0300

    feat(CategoryCrudTest): add tests for Category

--- tests/Feature/Category/CategoryCrudTest.php
+++ tests/Feature/Category/CategoryCrudTest.php
@@ -13,6 +13,7 @@ beforeEach(function () {
     $this->actingAs($user);
 });
 
+// CREATE
 it('should be able to create category', function () {
     $response = $this->post(route('categories.store'), [
         'name'  => 'New Category',
@@ -33,6 +34,14 @@ it('should be able to create category', function () {
     ]);
 });
 
+// READ
+it('should be able to view categories index', function () {
+    $response = $this->get(route('categories.index'));
+
+    $response->assertStatus(200);
+});
+
+// EDIT
 it('should be able to edit category', function () {
     $category = Category::factory()->create([
         'user_id' => auth()->id(),
@@ -55,4 +64,34 @@ it('should be able to edit category', function () {
         'icon'  => CategoryIcon::CAR->value,
         'type'  => TransactionType::INCOME->value,
     ]);
+});
+
+it('shoud not be able to edit category that you do not own', function () {
+    $category = Category::factory()->create();
+
+    $response = $this->put(route('categories.update', $category->id), [
+        'name' => 'Updated Category',
+        'type' => TransactionType::INCOME->value,
+    ]);
+
+    $response->assertStatus(403);
+});
+
+// DELETE
+it('should be able to delete category', function () {
+    $category = Category::factory()->create([
+        'user_id' => auth()->id(),
+    ]);
+
+    $response = $this->delete(route('categories.destroy', $category->id));
+
+    $response->assertRedirect(route('categories.index'));
+});
+
+it('should not be able to delete category that you do not own', function () {
+    $category = Category::factory()->create();
+
+    $response = $this->delete(route('categories.destroy', $category->id));
+
+    $response->assertStatus(403);
 });
